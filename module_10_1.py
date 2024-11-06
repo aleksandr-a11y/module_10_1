b7d89f7da02a426554b13e8fb177b950a94eba00
ПК <@DESKTOP-H9D4HFD>
2024-11-06 19:40:03 +0700

    Добавьте файлы проекта.

---
+++ module_10_1.py
@@ -0,0 +1,37 @@
+﻿import time   
+from datetime import datetime
+from threading import Thread
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(1, word_count + 1):
+            file.write(f'Какое-то слово № {i}\n')
+            time.sleep(0.1)
+    file.close()
+    print(f'Завершилась запись в файл {file_name}')
+
+start1 = datetime.now()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+stop1 = datetime.now()
+print(f'Работа потоков {stop1 - start1}')
+
+start2 = datetime.now()
+thread_1 = Thread(target=write_words, args=(10, 'example5.txt'))
+thread_2 = Thread(target=write_words, args=(30, 'example6.txt'))
+thread_3 = Thread(target=write_words, args=(200, 'example7.txt'))
+thread_4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+thread_1.start()
+thread_2.start()
+thread_3.start()
+thread_4.start()
+
+thread_1.join()
+thread_2.join()
+thread_3.join()
+thread_4.join()
+stop2 = datetime.now()
+print(f'Работа потоков {stop2 - start2}')
